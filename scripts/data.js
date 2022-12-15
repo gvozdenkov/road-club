494fae46118e6b73c89a564984413a1de0183341
arty gvozdenkov <gvozdenkov@gmail.com>
2022-12-15 09:44:37 +0300

    init commit. Starter image pack, data, fonts, favicon, scss

---
+++ scripts/data.js
@@ -0,0 +1,62 @@
+const bikes = [
+  {
+    type: "highway",
+    name: "Cervelo Caledonia-5",
+    link: "../images/bike-type/highway/cervelo-caledonia-5.jpg",
+  },
+  {
+    type: "highway",
+    name: "Cannondale Systemsix Himod",
+    link: "../images/bike-type/highway/cannondale-systemsix-imod.jpg",
+  },
+  {
+    type: "highway",
+    name: "Trek Domane SL-7",
+    link: "../images/bike-type/highway/trek-domane-sl-7.jpg",
+  },
+  {
+    type: "gravel",
+    name: "Cervelo Aspero GRX 810",
+    link: "../images/bike-type/highway/cervelo-aspero-grx-rx810.jpg",
+  },
+  {
+    type: "gravel",
+    name: "Specialized S-Works Diverge",
+    link: "../images/bike-type/highway/specialized-s-works-diverge.jpg",
+  },
+  {
+    type: "gravel",
+    name: "Cannondale Topstone Lefty 3",
+    link: "../images/bike-type/highway/cannondale-topstone-lefty-3.jpg",
+  },
+  {
+    type: "triathlon",
+    name: "Specialized S-Works Shiv",
+    link: "../images/bike-type/highway/specialized-s-works-shiv.jpg",
+  },
+  {
+    type: "triathlon",
+    name: "BMC Timemachine 01 ONE",
+    link: "../images/bike-type/highway/bmc-timemachine-01-one.png",
+  },
+  {
+    type: "triathlon",
+    name: "Cervelo P-Series",
+    link: "../images/bike-type/highway/cervelo-p-series.webp",
+  },
+];
+
+const tracks = [
+  {
+    type: "highway",
+    desc: "На шоссейном велосипеде можно ездить по асфальту на разных градиентах: будь то горы или равнины. Гонки проходят в командном пелотоне, но тренироваться можно и самостоятельно.",
+  },
+  {
+    type: "gravel",
+    desc: "Грэвел похож на шоссейный велосипед, но конструкция рамы немного отличается, и на нём стоят более широкие покрышки, всё для того чтобы проехать по лёгкому бездорожью.",
+  },
+  {
+    type: "triathlon",
+    desc: "ТТ — это велосипед для триатлона или раздельного старта, гооняют на таком велике только по равнинному асфальту, велик очень быстрые и аэродинамичный.",
+  },
+];
